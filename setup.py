28c4c4cd662fb32d762945fbb949f1c451908af0
Roy Hyunjin Han <rhh@crosscompute.com>
2019-02-17 11:07:28 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'CHANGES.rst', 'README.rst'])
 setup(
     name='crosscompute-geotable',
-    version='0.7.7.3',
+    version='0.7.7.4',
     description='GeoTable data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
